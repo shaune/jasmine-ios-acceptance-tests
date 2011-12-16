f465d70eeb644c71ab4759c9611d5a57519f62ce
shaun <shaun.ervine@sensis.com.au>
2011-12-16 11:55:40 +1100

    README change

---
+++ build.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+TRACETEMPLATE="/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate"
+BASE_TEST_SCRIPT=$1
+APP_LOCATION=$2
+
+if [ ! $# -gt 1 ]; then
+	echo "You must specify the app location and the test file."
+	echo "\t eg. ./build.sh suite.js <xcodeproject directory>/build/Debug-iphonesimulator/myapp.app"
+	exit -1
+fi
+
+# Create junit reporting directory
+if [ ! -d "test-reports" ]; then
+  mkdir test-reports
+fi
+
+# Kick off the instruments build
+instruments -t $TRACETEMPLATE $APP_LOCATION -e UIASCRIPT $BASE_TEST_SCRIPT -e UIARESULTSPATH /var/tmp | grep "<"  > test-reports/test-results.xml
+
+# cleanup the tracefiles produced from instruments
+rm -rf *.trace
+
+# fail script if any failures have been generated
+if [ `grep "<failure>" test-reports/test-results.xml | wc -l` -gt 0 ]; then
+        echo 'Build Failed'
+        exit -1
+else
+        echo 'Build Passed'
+        exit 0
+fi
+
+
---
+++ ci.js
@@ -0,0 +1,11 @@
+// load dependencies
+#import "ext/jasmine-uiautomation.js"
+#import "ext/jasmine-reporters/ext/jasmine.js"
+#import "ext/reporter/jasmine.uiautomation_junit_reporter.js"
+#import "ext/jasmine-main-thread.js"
+
+// load test specs
+#import "specs/my-first-uiautomation-test.js"
+
+jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter());
+jasmine.getEnv().execute();
